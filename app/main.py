7ac786beb43ac182b12417f4908062953b7b4c65
Sri venkatesha mani <srivenkateshmani@gmail.com>
2026-09-16 23:22:07 -0500

    Add database connection, schema, fastapi connection

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.database import engine
+from app.models import Base
 
 app = FastAPI()
+Base.metadata.create_all(bind = engine)
 
 @app.get("/ping")
 def ping():
